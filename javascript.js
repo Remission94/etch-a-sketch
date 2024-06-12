7e783ed9860193ced8c7e628877d6d31c666973d
Remission94 <vmajkic94@gmail.com>
2024-06-12 21:13:20 +0200

    Grid adjustments are now working, but need to add hover function to them

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 const container = document.querySelector(".container");
 
+
 for (let i = 0; i < 16; i++) {
     for (let i = 0; i < 16; i++) {
         const div = document.createElement("div");
@@ -7,16 +8,48 @@ for (let i = 0; i < 16; i++) {
         div.setAttribute("style", "height: 48px; width: 48px; border: black solid 1px");
         container.appendChild(div)
     }
-
 }
 
-function hoverColor () {
-    let gridElements = document.querySelectorAll(".gridElement");
+let gridElements = document.querySelectorAll(".gridElement");
+
+function clear () {
+    gridElements.forEach(element => {
+        element.style.backgroundColor = "white"})
+}
+
+function newGrid () {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+      }
+    let input = document.querySelector("#numOfSquares")
+    let inputValue = input.value
+    let newDim = 800/inputValue-2+"px";
+    for (let i = 0; i < inputValue; i++) {
+        for (let i = 0; i < inputValue; i++) {
+            const div = document.createElement("div");
+            div.classList.add("gridElement")
+            div.style.border = "black solid 1px";
+            div.style.height = newDim
+            div.style.width = newDim
+            container.appendChild(div)
+        }
+    }
+}
+
+
+function hoverColor () {  
     gridElements.forEach(element => {
         element.addEventListener("mouseover", () => {
             element.style.backgroundColor = "red"})
     })
 }
 
+
+
+const clearBtn = document.querySelector("#clear");
+clearBtn.addEventListener("click", clear)
+
+const enterBtn = document.querySelector("#enter");
+enterBtn.addEventListener("click", newGrid)
 
 hoverColor()
